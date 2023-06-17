8f3ce25be0579b73321dba7762a37599ec7512b6
nasrinb <nsrn.bagheri@gmail.com>
2023-06-17 11:24:38 +0330

    add conditional list display

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,22 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
-  const[title,setTitle]=useState(props.title)
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    setTitle('updated');
+    setTitle("updated");
     console.log("clicked!");
   };
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Change Title</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Change Title</button>
+      </Card>
+    </li>
   );
 }
 
